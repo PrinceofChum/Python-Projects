ed18e1d21014e5438f73812dbb9af3f59744f2b8
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-03-12 22:24:03 +0530

    Update Random Password.py

--- Password Generator/Random Password.py
+++ Password Generator/Random Password.py
@@ -4,7 +4,7 @@ import random
 def randompassword():
     chars = string.ascii_letters + string.digits
     size = 3
-    return ''.join(random.choice(chars) for x in range(size,27))
+    return ''.join(random.choice(chars) for x in range(size,15))
 
 n = 0
 while n < 50:
